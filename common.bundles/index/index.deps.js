f27f4e30690750ad6f9dcd588e4ce2882189d8a5
Artem Anashev <anashev@yamoney.ru>
2016-12-12 14:48:04 +0300

    Заменены табы на пробелы

--- common.bundles/index/index.deps.js
+++ common.bundles/index/index.deps.js
@@ -10,29 +10,9 @@ exports.deps = [
         "elem": "col"
     },
     {
-        "block": "row",
-        "elem": "col",
-        "mod": "sw"
-    },
-    {
         "block": "header"
     },
     {
         "block": "row"
-    },
-    {
-        "block": "row",
-        "elem": "col",
-        "mod": "sw",
-        "val": 2
-    },
-    {
-        "block": "row",
-        "mod": "order"
-    },
-    {
-        "block": "row",
-        "mod": "order",
-        "val": "reverse"
     }
 ];
